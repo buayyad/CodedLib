0fe89de559e8c85cf4ceb51724c3fc2b44f70010
Abdullah Al-Rashidi <alrashidi@nasaqagency.com>
2025-08-31 19:27:21 +0300

    Up to card 2

---
+++ src/connection.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+export async function connectToDatabase() {
+  const uri = process.env.MONGODB_URI;
+  if (!uri) {
+    console.error("MongoDB URI is not defined.");
+    process.exit();
+  }
+  try {
+    await mongoose.connect(uri);
+    console.log("MongoDB connection successful");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    process.exit();
+  }
+}
